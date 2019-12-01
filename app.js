f6de67d73fc45205808b0bcee6902ca2fa875eb3
Thang <shadercatin@gmail.com>
2019-12-01 21:07:25 +0200

    trying to set SameSite property for express session 2

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     res.append('Access-Control-Allow-Credentials', 'true');
+    res.append('sameSite', 'none');
     next();
 });
 app.use('/', indexRouter);
